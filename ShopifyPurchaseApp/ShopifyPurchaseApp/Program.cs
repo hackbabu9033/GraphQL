2fdf98b1f6017eba87ec7422aa3ba88b85c6afe2
Hank Lin <HankLin@nine-yi.com>
2023-03-03 16:10:21 +0800

    補上prototype

--- ShopifyPurchaseApp/ShopifyPurchaseApp/Program.cs
+++ ShopifyPurchaseApp/ShopifyPurchaseApp/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace ShopifyPurchaseApp
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace ShopifyPurchaseApp
 {
     internal class Program
     {
+        private static string shopifyApiEndPoint = "https://eat-your-own-dog-food.myshopify.com/api/2022-10/graphql.json";
         static void Main(string[] args)
         {
-            var shopifyApiEndPoint = "https://eat-your-own-dog-food.myshopify.com/api/2022-10/graphql.json";
-            var accessToken = string.Empty;
             var httpClient = new HttpClient();
+            var accessToken = string.Empty;
 
             // 選擇是否登入
             // Y:打accesstokenApi取得customerAccessToken
@@ -19,13 +22,57 @@
             {
                 var email = Console.ReadLine();
                 var pwd = Console.ReadLine();
+                var httpClinet = new HttpClient();
+                
             }
 
-            // 陳列商品內容(考慮是否顯式分頁)
+            // 陳列商品內容(考慮是否顯示分頁)
+            
 
             // 將輸入選擇的商品內容放到collection裡面
 
             // 建立購物車連結，取得checkout url
+        }
+
+        static string GetAccessToken(string email,string pwd)
+        {
+            var noMail = string.IsNullOrWhiteSpace(email);
+            var noPwd = string.IsNullOrWhiteSpace(pwd);
+            if (noMail || noPwd)
+            {
+                throw new ArgumentNullException($"mail and pwd are required");
+            }
+            var request = new
+            {
+                // query給字串
+                // variable給object
+                query = @"mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
+                          customerAccessTokenCreate(input: $input) {
+                            customerAccessToken {
+                                accessToken
+                                expiresAt
+                            }
+                            customerUserErrors {
+                              code
+                              field
+                              message
+                            }
+                          }
+                        }",
+                // 這裡不要序列化，因為後面會做一次，這裡做的話會變成多序列化一次
+                variables = new 
+                { 
+                    input = new{ 
+                        email = email,
+                        pwd = pwd
+                    }
+                },
+            };
+            var addBookBody = JsonConvert.SerializeObject(request);
+            var addBookHttpContent = new StringContent(addBookBody, Encoding.UTF8, "application/json");
+            var httpClinet = new HttpClient();
+            httpClinet.PostAsync(shopifyApiEndPoint, addBookHttpContent);
+            return string.Empty;
         }
     }
 }
---
+++ ShopifyPurchaseApp/ShopifyPurchaseApp/ResponseModel/CustomerAccessTokenCreate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifyPurchaseApp.ResponseModel
+{
+    internal class CustomerAccessTokenCreate
+    {
+       
+        public Token customerAccessToken { get; set; }
+
+        public ErrorRespone customerUserErrors { get;set; }
+
+        public class Token
+        {
+            public string AccessToken { get; set; }
+            public DateTimeOffset ExpiresAt { get; set; }
+        }
+        public class ErrorRespone
+        {
+            
+        }
+    }
+
+   
+}
